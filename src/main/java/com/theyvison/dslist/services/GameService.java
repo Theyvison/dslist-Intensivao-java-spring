1d8e32eb2b42cfc636b5572e4c3da3c1ad7115de
Theyvison <theyvison.jf@hotmail.com>
2025-08-24 11:16:27 -0300

    get game by id

--- src/main/java/com/theyvison/dslist/services/GameService.java
+++ src/main/java/com/theyvison/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.theyvison.dslist.services;
 
+import com.theyvison.dslist.dto.GameDTO;
 import com.theyvison.dslist.dto.GameMinDTO;
 import com.theyvison.dslist.entities.Game;
 import com.theyvison.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,14 @@ public class GameService {
     @Autowired // injeta a dependência
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true) // transactional -> gerencia a transação com o banco de dados, readOnly -> não faz lock no banco de dados
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO gameDTO = new GameDTO(result);
+        return gameDTO;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(game -> new GameMinDTO(game)).toList();
